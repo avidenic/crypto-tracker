ca440a7edb3b2617125fc503a9decc065e54139c
avidenic <avidenic@users.noreply.github.com>
2020-01-24 22:34:09 +0100

    add docker support (#5)
    * Added docker support and readme
    
    * added a bit better instructions on how to run the app

--- src/app/coins/coins-page/coins-page.component.ts
+++ src/app/coins/coins-page/coins-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 import { CoinsService } from '../coins.service';
 import { Coin } from '../coins';
 
